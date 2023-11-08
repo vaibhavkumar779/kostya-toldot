25a5bc44ebccc9b7f6710aad931c99d3203c5f2e
VAIBHAV KUMAR <vaibhav.kumar@nashtechglobal.com>
2023-11-08 20:13:05 +0530

    added gunicorn file

---
+++ server/gunicorn.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
+accesslog = "-"
+access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
